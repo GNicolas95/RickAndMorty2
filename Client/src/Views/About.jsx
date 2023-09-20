543f66583325336a2fef1347f7bf6ae1e27489a7
GNicolas95 <geroma16@gmail.com>
2023-09-20 16:20:23 -0300

    Avances

--- Client/src/Views/About.jsx
+++ Client/src/Views/About.jsx
@@ -7,8 +7,8 @@ function About() {
                 Bienvenidos a mi primer app con React !!!</h1>
             <h1 className={styles.aboutTitle}>
             Mi nombre es Gerónimo Nicolás Almonte. Actualmente, me encuentro cursando la 
-            carrera de Desarrollo FullStack en Henry. Esta aplicación es una pequeña muestra
-            de algunas herramientas que fuimos adquiriendo en el bootcamp. 
+            carrera de Desarrollo FullStack en Henry. Esta aplicación es una pequeña muestra 
+            de lo que podemos lograr con algunas de las herramientas que fuimos adquiriendo en el bootcamp. 
             </h1> 
         </div>
     )

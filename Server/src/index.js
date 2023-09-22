65037f84d747d644bb1abb095a3614519cbaba48
GNicolas95 <geroma16@gmail.com>
2023-09-22 15:05:44 -0300

    Resolucion

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,9 @@
-const server = require("./src/app")
+const server = require("./app");
+const {conn} = require("./DB_connection");
 
 const PORT = 3001;
+
+conn.sync({force:true})
 
 server.listen(PORT, () => {
   console.log("Server raised in port: " + PORT);
